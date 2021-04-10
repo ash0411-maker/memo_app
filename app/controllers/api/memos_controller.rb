d695c060445143635d2804ef511a9738ed60b698
ash <satoshi.0411@icloud.com>
2021-04-10 21:19:44 +0900

    [add] show

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -1,5 +1,21 @@
 class Api::MemosController < ApplicationController
+  protect_from_forgery
+
   def index
     @memos = Memo.order('created_at DESC')
   end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      render :show, status: :created
+    else
+      render json: @memo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def memo_params
+      params.require(:memo).permit(:title, :description)
+    end
 end
